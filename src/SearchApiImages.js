4a934fb3df178b300ca6f9c50f403fd98e32d880
Oleksii Korolenko <as_vostok@i.ua>
2023-02-11 23:00:54 +0200

    add task 1

---
+++ src/SearchApiImages.js
@@ -0,0 +1,31 @@
+const KEY = '33551348-9d68666fc5ce894df97e3b30d';
+const ENDPOINT = 'https://pixabay.com/api/';
+
+export default class SearchApiImages {
+  constructor() {
+    this.page = 1;
+    this.searchQuery = '';
+  }
+
+  getImages() {
+    const URL = `${ENDPOINT}?key=${KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=5&page=${this.page}`;
+
+    return fetch(URL).then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+
+      return response.json();
+    }).then(data => {
+      return data.hits
+    });
+  }
+
+  nextPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,78 @@
-console.log(555);
+import Notiflix from 'notiflix';
+import SearchApiImages from './SearchApiImages.js';
+
+const form = document.querySelector('.search-form');
+const searchBtn = document.querySelector('[type="submit"]');
+const gallery = document.querySelector('.gallery');
+console.log('gallery', gallery);
+
+const searchApiImages = new SearchApiImages();
+
+form.addEventListener('submit', searchImg);
+
+function searchImg(e) {
+  e.preventDefault();
+
+  const form = e.currentTarget;
+  const value = form.elements.search.value.trim();
+
+  searchApiImages.searchQuery = value;
+
+  searchApiImages
+    .getImages()
+    .then(arr => {
+      if (arr.length === 0) {
+        Notiflix.Notify.info(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        return;
+      }
+      console.log(arr);
+
+      createImagesCollection(arr);
+    })
+    .catch(error => {
+      console.error(error);
+      Notiflix.Notify.failure(`${error}`);
+    });
+}
+
+function createImagesCollection(arr) {
+  const markupImagesCollectiom = arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" width="640" height="427" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>${downloads}
+    </p>
+  </div>
+</div>`;
+      }
+    )
+    .join('');
+  // console.log(markupImagesCollectiom);
+  gallery.innerHTML = markupImagesCollectiom;
+}
+
+function clearImagesWrapper() {
+  imagesWrapper.innerHTML = '';
+}
