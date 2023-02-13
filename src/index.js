ed62364691803c6f5d6d7c0116ee0549225804ed
Oleksii Korolenko <as_vostok@i.ua>
2023-02-13 19:23:48 +0200

    add correct wrap images

--- src/index.js
+++ src/index.js
@@ -92,21 +92,18 @@ function createImagesCollection(arr) {
         downloads,
       }) => {
         return `<div class="photo-card">
-      <a href="${largeImageURL}">
-        <img
-          src="${webformatURL}"
-          alt="${tags}"
-          loading="lazy"
-          width="640"
-          height="427"
-      /></a>
-      <div class="info">
-        <p class="info-item"><b>Likes</b>${likes}</p>
-        <p class="info-item"><b>Views</b>${views}</p>
-        <p class="info-item"><b>Comments</b>${comments}</p>
-        <p class="info-item"><b>Downloads</b>${downloads}</p>
-      </div>
-    </div>`;
+  <div class="wrap-photo">
+    <a href="${largeImageURL}">
+    <img src="${webformatURL}" alt="${tags}" loading="lazy" width="640"
+  /></a>
+  </div>
+  <div class="info">
+    <p class="info-item"><b>Likes</b>${likes}</p>
+    <p class="info-item"><b>Views</b>${views}</p>
+    <p class="info-item"><b>Comments</b>${comments}</p>
+    <p class="info-item"><b>Downloads</b>${downloads}</p>
+  </div>
+</div>`;
       }
     )
     .join('');
